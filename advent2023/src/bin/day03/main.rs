f02dcbc0666b5b6987acd33ce3f668d0e46592af
Brad Warren <bradmwarren@gmail.com>
2024-05-15 15:54:08 -0700

    finish day03

--- advent2023/src/bin/day03/main.rs
+++ advent2023/src/bin/day03/main.rs
@@ -1,12 +1,41 @@
 use std::cmp::min;
+use std::collections::HashMap;
+use std::ops::Range;
+use std::str::from_utf8;
+
+type ByteGrid<'a> = Vec<&'a [u8]>;
 
 const PERIOD_AS_U8: u8 = ".".as_bytes()[0];
+const GEAR_AS_U8: u8 = "*".as_bytes()[0];
 
-fn build_grid(input: &str) -> Vec<&[u8]> {
+fn build_grid(input: &str) -> ByteGrid {
     input.lines().map(|line| line.as_bytes()).collect()
 }
 
-fn num_borders_symbol(input: &[&[u8]], row_index: usize, col_start: usize, col_end: usize) -> bool {
+fn find_number_indices(input: &ByteGrid) -> Vec<(usize, Range<usize>)> {
+    input
+        .iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            let mut iter = 0..row.len();
+            let mut nums = Vec::new();
+            while let Some(start) = iter.find(|&j| row[j].is_ascii_digit()) {
+                let end = iter
+                    .find(|&j| !row[j].is_ascii_digit())
+                    .unwrap_or(row.len());
+                nums.push((i, (start..end)));
+            }
+            nums
+        })
+        .collect()
+}
+
+fn get_border_indices(
+    input: &[&[u8]],
+    row_index: usize,
+    col_start: usize,
+    col_end: usize,
+) -> Vec<(usize, usize)> {
     let row_start = row_index.saturating_sub(1);
     let row_end = min(row_index + 2, input.len());
     let col_start = col_start.saturating_sub(1);
@@ -15,34 +44,72 @@ fn num_borders_symbol(input: &[&[u8]], row_index: usize, col_start: usize, col_e
             let col_end = min(col_end + 1, input[i].len());
             (col_start..col_end).map(move |j| (i, j))
         })
-        .any(|(i, j)| {
-            let byte = input[i][j];
-            !byte.is_ascii_digit() && byte != PERIOD_AS_U8
-        })
+        .collect()
+}
+
+fn has_border_symbol(input: &[&[u8]], row_index: usize, col_start: usize, col_end: usize) -> bool {
+    let border = get_border_indices(input, row_index, col_start, col_end);
+    border.into_iter().any(|(i, j)| {
+        let byte = input[i][j];
+        !byte.is_ascii_digit() && byte != PERIOD_AS_U8
+    })
 }
 
 fn part1(input: &str) -> u32 {
     let input = build_grid(input);
-    input.iter().enumerate().fold(0, |mut acc, (i, row)| {
-        let mut iter = 0..row.len();
-        while let Some(start) = iter.find(|&j| row[j].is_ascii_digit()) {
-            let end = iter
-                .find(|&j| !row[j].is_ascii_digit())
-                .unwrap_or(row.len());
-            if num_borders_symbol(&input, i, start, end) {
-                acc += std::str::from_utf8(&row[start..end])
-                    .unwrap()
-                    .parse::<u32>()
-                    .unwrap();
+    let number_indices = find_number_indices(&input);
+    number_indices
+        .into_iter()
+        .filter_map(|(row, cols)| {
+            if has_border_symbol(&input, row, cols.start, cols.end) {
+                Some(
+                    from_utf8(&input[row][cols])
+                        .unwrap()
+                        .parse::<u32>()
+                        .unwrap(),
+                )
+            } else {
+                None
             }
-        }
-        acc
-    })
+        })
+        .sum()
+}
+
+fn get_border_gears(
+    input: &[&[u8]],
+    row_index: usize,
+    col_start: usize,
+    col_end: usize,
+) -> Vec<(usize, usize)> {
+    let border = get_border_indices(input, row_index, col_start, col_end);
+    border
+        .into_iter()
+        .filter(|(i, j)| input[*i][*j] == GEAR_AS_U8)
+        .collect()
 }
 
 fn part2(input: &str) -> u32 {
-    let _input = build_grid(input);
-    42
+    let input = build_grid(input);
+    let number_indices = find_number_indices(&input);
+    let mut gear_nums: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    for (row, cols) in number_indices {
+        let col_start = cols.start;
+        let col_end = cols.end;
+        let num = from_utf8(&input[row][cols]).unwrap().parse().unwrap();
+        for indices in get_border_gears(&input, row, col_start, col_end) {
+            gear_nums.entry(indices).or_default().push(num);
+        }
+    }
+    gear_nums
+        .values()
+        .filter_map(|v| {
+            if v.len() == 2 {
+                Some(v.iter().product::<u32>())
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn main() {
@@ -60,9 +127,9 @@ mod day03 {
         assert_eq!(part1(include_str!("input")), 544664);
     }
 
-    //#[test]
-    //fn test_part2() {
-    //    assert!(part2(include_str!("example2")) == 281);
-    //    assert!(part2(include_str!("input")) == 54770);
-    //}
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(include_str!("example")), 467835);
+        assert_eq!(part2(include_str!("input")), 84495585);
+    }
 }
