6a27184862dbce389de650af82de9a168f0d6dfc
Brad Warren <bradmwarren@gmail.com>
2024-05-16 22:51:02 -0700

    first attempt at 5.2

--- advent2023/src/bin/day05/main.rs
+++ advent2023/src/bin/day05/main.rs
@@ -1,3 +1,4 @@
+use std::error;
 use std::ops::Range;
 
 #[derive(Clone, Debug)]
@@ -15,27 +16,26 @@ impl MapRange {
     }
 }
 
-fn get_seeds(seed_line: &str) -> Vec<u64> {
-    seed_line
-        .split_whitespace()
-        .skip(1)
-        .map(|i| i.parse().unwrap())
+impl TryFrom<&str> for MapRange {
+    type Error = Box<dyn error::Error>;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut v = Vec::new();
+        for i in value.split_whitespace() {
+            v.push(i.parse()?);
+        }
+        assert_eq!(v.len(), 3);
+        Ok(MapRange::new(v[0], v[1], v[2]))
+    }
+}
+
+fn build_map(map_lines: &[&str]) -> Vec<MapRange> {
+    map_lines
+        .iter()
+        .map(|line| MapRange::try_from(*line).unwrap())
         .collect()
 }
 
-fn build_map<'a>(iter: &mut impl Iterator<Item = &'a str>) -> Vec<MapRange> {
-    let iter = iter.take_while(|s| !s.is_empty());
-    let mut result = Vec::new();
-    for s in iter {
-        let values: Vec<u64> = s.split_whitespace().map(|i| i.parse().unwrap()).collect();
-        assert_eq!(values.len(), 3);
-        result.push(MapRange::new(values[0], values[1], values[2]));
-    }
-    result
-}
-
-fn apply_map<'a>(iter: &mut impl Iterator<Item = &'a str>, seeds: &mut Vec<u64>) {
-    let map = build_map(iter);
+fn apply_map(map: &[MapRange], seeds: &mut [u64]) {
     for seed in seeds {
         if let Some(range) = map.iter().find(|r| r.src.contains(seed)) {
             *seed = *seed - range.src.start + range.dest_start;
@@ -43,20 +43,43 @@ fn apply_map<'a>(iter: &mut impl Iterator<Item = &'a str>, seeds: &mut Vec<u64>)
     }
 }
 
-fn part1(input: &str) -> u64 {
-    let mut iter = input.lines();
-    let seed_line = iter.next().unwrap();
-    let mut seeds = get_seeds(seed_line);
-    // skip blank line after seed line
-    _ = iter.next();
-    while iter.next().is_some() {
-        // the above iter.next() call skips the map header
-        apply_map(&mut iter, &mut seeds);
+fn do_part(input: &str, seed_fn: impl Fn(&str) -> Vec<u64>) -> u64 {
+    let mut iter = input.lines().filter(|line| !line.is_empty()).peekable();
+    let mut seeds = seed_fn(iter.next().unwrap());
+
+    while iter.peek().is_some() {
+        let map_lines: Vec<&str> = iter
+            .by_ref()
+            .take_while(|line| line.as_bytes()[0].is_ascii_digit())
+            .collect();
+        apply_map(&build_map(&map_lines), &mut seeds);
     }
     *seeds.iter().min().unwrap()
 }
 
+fn part1(input: &str) -> u64 {
+    do_part(input, |seed_line| {
+        seed_line
+            .split_whitespace()
+            .skip(1)
+            .map(|i| i.parse().unwrap())
+            .collect()
+    })
+}
+
 fn part2(_input: &str) -> u64 {
+    // this is WAY too slow. generating the seeds is pretty bad, but what's really bad is applying
+    // the map n times. try merging maps into one that maps initial input to final output? even
+    // then, i think i'll need to do something clever to find the min output without trying all
+    // inputs
+    //do_part(input, |seed_line| {
+    //    let v: Vec<_> = seed_line.split_whitespace()
+    //        .skip(1)
+    //        .map(|i| i.parse().unwrap()).collect();
+    //    v.chunks(2)
+    //        .flat_map(|chunk| (chunk[0]..chunk[0] + chunk[1]))
+    //        .collect()
+    //})
     42
 }
 
