a93d13ccb1bef91e4b511bdeddeedc0dc9f14e3b
Brad Warren <bradmwarren@gmail.com>
2024-05-21 10:51:24 -0700

    add gross 5.2

--- advent2023/src/bin/day05/main.rs
+++ advent2023/src/bin/day05/main.rs
@@ -1,18 +1,40 @@
+use std::cmp::{max, min};
 use std::error;
 use std::ops::Range;
 
-#[derive(Clone, Debug)]
+type Map = Vec<MapRange>;
+type SeedRanges = Vec<Range<u64>>;
+
+#[derive(Clone, Copy, Debug)]
 struct MapRange {
     dest_start: u64,
-    src: Range<u64>,
+    src_start: u64,
+    len: u64,
 }
 
 impl MapRange {
     fn new(dest_start: u64, src_start: u64, len: u64) -> Self {
-        MapRange {
+        Self {
             dest_start,
-            src: (src_start..src_start + len),
+            src_start,
+            len,
         }
+    }
+
+    fn dest_contains(&self, num: u64) -> bool {
+        num >= self.dest_start && num < self.dest_end()
+    }
+
+    fn dest_end(&self) -> u64 {
+        self.dest_start + self.len
+    }
+
+    fn src_contains(&self, num: u64) -> bool {
+        num >= self.src_start && num < self.src_end()
+    }
+
+    fn src_end(&self) -> u64 {
+        self.src_start + self.len
     }
 }
 
@@ -28,7 +50,7 @@ impl TryFrom<&str> for MapRange {
     }
 }
 
-fn build_map(map_lines: &[&str]) -> Vec<MapRange> {
+fn build_map(map_lines: &[&str]) -> Map {
     map_lines
         .iter()
         .map(|line| MapRange::try_from(*line).unwrap())
@@ -37,50 +59,128 @@ fn build_map(map_lines: &[&str]) -> Vec<MapRange> {
 
 fn apply_map(map: &[MapRange], seeds: &mut [u64]) {
     for seed in seeds {
-        if let Some(range) = map.iter().find(|r| r.src.contains(seed)) {
-            *seed = *seed - range.src.start + range.dest_start;
+        if let Some(range) = map.iter().find(|r| r.src_contains(*seed)) {
+            *seed = *seed - range.src_start + range.dest_start;
         }
     }
 }
 
-fn do_part(input: &str, seed_fn: impl Fn(&str) -> Vec<u64>) -> u64 {
-    let mut iter = input.lines().filter(|line| !line.is_empty()).peekable();
-    let mut seeds = seed_fn(iter.next().unwrap());
+fn find_intersection(map1: &Map, map2: &Map) -> Option<(usize, usize)> {
+    for (i, r1) in map1.iter().enumerate() {
+        for (j, r2) in map2.iter().enumerate() {
+            if r1.src_contains(r2.src_start) || r2.src_contains(r1.src_start) {
+                return Some((i, j))
+            }
+        }
+    }
+    None
+}
 
+fn merge_if_no_overlap(new_map: &mut Map, old_map: &Map) {
+    for mut old_ranges in old_map.iter().map(|r| vec![*r]) {
+        while let Some((new_i, old_i)) = find_intersection(new_map, &old_ranges) {
+            let old_range = old_ranges.swap_remove(old_i);
+            let new_range = new_map[new_i];
+            let overlap_start = max(old_range.src_start, new_range.src_start);
+            let overlap_end = min(old_range.src_end(), new_range.src_end());
+            let overlap_len = overlap_end - overlap_start;
+            if old_range.len - overlap_len > 0 {
+                if old_range.src_start < new_range.src_start {
+                    let len = new_range.src_start - old_range.src_start;
+                    old_ranges.push(MapRange::new(old_range.dest_start, old_range.src_start, len));
+                }
+                if old_range.src_end() > new_range.src_end() {
+                    let len = old_range.src_end() - new_range.src_end();
+                    let offset = new_range.src_end() - old_range.src_start;
+                    let new_dst = old_range.dest_start + offset;
+                    old_ranges.push(MapRange::new(new_dst, new_range.src_end(), len));
+                }
+            }
+        }
+        new_map.extend(old_ranges);
+    }
+}
+
+fn update_map(old_map: &Map, new_map_lines: &[&str]) -> Map {
+    let new_map = build_map(new_map_lines);
+    let mut merged_map = Map::new();
+    for new_range in &new_map {
+        for old_range in old_map {
+            if old_range.dest_contains(new_range.src_start)
+                || new_range.src_contains(old_range.dest_start)
+            {
+                let overlap_start = max(old_range.dest_start, new_range.src_start);
+                let overlap_end = min(old_range.dest_end(), new_range.src_end());
+                let overlap_len = overlap_end - overlap_start;
+                let old_offset = overlap_start - old_range.dest_start;
+                let new_offset = overlap_start - new_range.src_start;
+                merged_map.push(MapRange::new(
+                    new_range.dest_start + new_offset,
+                    old_range.src_start + old_offset,
+                    overlap_len,
+                ));
+            }
+        }
+    }
+    merge_if_no_overlap(&mut merged_map, &old_map);
+    merge_if_no_overlap(&mut merged_map, &new_map);
+    merged_map
+}
+
+fn seeds_to_test(seed_ranges: &SeedRanges, map: &Map) -> Vec<u64> {
+    let mut v = Vec::new();
+    for map_range in map {
+        for seed_range in seed_ranges {
+            if seed_range.contains(&map_range.src_start) || map_range.src_contains(seed_range.start) {
+                v.push(max(map_range.src_start, seed_range.start));
+            }
+        }
+    }
+    for seed_range in seed_ranges {
+        v.push(seed_range.start);
+    }
+    v
+}
+
+fn do_part<T: Fn(&str) -> SeedRanges>(input: &str, seed_fn: T) -> u64 {
+    let mut iter = input.lines().filter(|line| !line.is_empty()).peekable();
+    let seed_ranges = seed_fn(iter.next().unwrap());
+    let mut map = Map::new();
+
+    let _ = iter.next();
     while iter.peek().is_some() {
         let map_lines: Vec<&str> = iter
             .by_ref()
             .take_while(|line| line.as_bytes()[0].is_ascii_digit())
             .collect();
-        apply_map(&build_map(&map_lines), &mut seeds);
+        map = update_map(&map, &map_lines);
     }
+    let mut seeds = seeds_to_test(&seed_ranges, &map);
+    apply_map(&map, &mut seeds);
     *seeds.iter().min().unwrap()
 }
 
-fn part1(input: &str) -> u64 {
-    do_part(input, |seed_line| {
-        seed_line
-            .split_whitespace()
-            .skip(1)
-            .map(|i| i.parse().unwrap())
-            .collect()
-    })
+fn seed_line_to_ints(line: &str) -> Vec<u64> {
+    line.split_whitespace()
+        .skip(1)
+        .map(|i| i.parse().unwrap())
+        .collect()
 }
 
-fn part2(_input: &str) -> u64 {
-    // this is WAY too slow. generating the seeds is pretty bad, but what's really bad is applying
-    // the map n times. try merging maps into one that maps initial input to final output? even
-    // then, i think i'll need to do something clever to find the min output without trying all
-    // inputs
-    //do_part(input, |seed_line| {
-    //    let v: Vec<_> = seed_line.split_whitespace()
-    //        .skip(1)
-    //        .map(|i| i.parse().unwrap()).collect();
-    //    v.chunks(2)
-    //        .flat_map(|chunk| (chunk[0]..chunk[0] + chunk[1]))
-    //        .collect()
-    //})
-    42
+fn parse_part1_seeds(line: &str) -> SeedRanges {
+    seed_line_to_ints(line).iter().map(|&i| (i..i+1)).collect()
+}
+
+fn part1(input: &str) -> u64 {
+    do_part(input, parse_part1_seeds)
+}
+
+fn parse_part2_seeds(line: &str) -> SeedRanges {
+    seed_line_to_ints(line).chunks(2).map(|chunk| (chunk[0]..chunk[0] + chunk[1])).collect()
+}
+
+fn part2(input: &str) -> u64 {
+    do_part(input, parse_part2_seeds)
 }
 
 fn main() {
@@ -98,9 +198,9 @@ mod day05 {
         assert_eq!(part1(include_str!("input")), 379811651);
     }
 
-    //#[test]
-    //fn test_part2() {
-    //    assert_eq!(part2(include_str!("example")), 30);
-    //    assert_eq!(part2(include_str!("input")), 10378710);
-    //}
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(include_str!("example")), 46);
+        assert_eq!(part2(include_str!("input")), 27992443);
+    }
 }
